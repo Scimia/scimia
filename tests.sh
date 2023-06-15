797d5c8a7e6f44f237b715a914c56d2a6b29fd11
Isa Farnik <isa@konghq.com>
2023-06-15 13:35:36 -0700

    wip

--- tests.sh
+++ tests.sh
@@ -1,3 +1,17 @@
 #!/usr/bin/env bash
 
+# strict mode
+set -euo pipefail
+IFS=$'\n\t'
+
+if [[ -n "${VERBOSE:-}" ]]; then
+  set -x
+fi
+
 sleep 3
+
+if [[ "$1" == 'flaky' ]]; then
+  rc="$(shuf -i 0-1 -n1)"
+  exit "${rc}"
+fi
+
